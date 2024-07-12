b672f889ec88ff8d16c8299490d1b20ca88bcbe2
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2024-07-12 19:38:22 +0600

    sign up login

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 require("dotenv").config();
@@ -23,11 +23,50 @@ async function run() {
   try {
     const db = client.db("simple-food");
     const productsCollection = db.collection("products");
+    const usersCollection = db.collection("user");
 
     app.get("/products", async (req, res) => {
       const products = await productsCollection.find({}).toArray();
       res.send({ status: true, data: products });
     });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id; 
+      const products = await productsCollection.findOne({ _id: new ObjectId(id)});
+      res.send({ status: true, data: products });
+    });
+
+    app.post("/signup", async (req, res) => {
+      const { name, email, password, number, address } = req.body;
+      const isExist = await usersCollection.findOne({email});
+      if(isExist) {
+        return res.send({ status: false, data:{message: 'user already exists'}})
+      }
+      await usersCollection.insertOne({
+        name,
+        email,
+        address,
+        number,
+        password
+      })
+      res.send({ status: true, data:{message: 'sign up successfully'}})
+    })
+    
+
+    app.post("/login", async (req, res) => {
+      const { email, password } = req.body;
+      const isExist = await usersCollection.findOne({email});
+      if(isExist) {
+        return res.send({ status: false, data:{message: 'user does not exists'}})
+      }
+      
+      if(isExist.password === password) {
+        return res.send({ status: false, data:{message: 'wrong password'}})
+      }
+
+      res.send({ status: true, data:{message: 'log in successfully'}})
+    })
+
 
   } finally {
 
